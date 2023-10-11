c2d0e71aec54776a99f5e1e13f4e62e540caea94
C1lowen <genukr5@ukr.net>
2023-10-11 17:38:10 +0300

    fixed bags

--- Users/src/main/java/com/users/users/repository/DataSource.java
+++ Users/src/main/java/com/users/users/repository/DataSource.java
@@ -3,7 +3,9 @@ package com.users.users.repository;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.yaml.snakeyaml.reader.StreamReader;
 
+import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -12,11 +14,22 @@ public class DataSource {
     private static HikariConfig config = new HikariConfig();
     private static HikariDataSource ds;
 
+    private static File file = new File("D:\\postInfo\\postgres.txt");
+
     static {
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
-        config.setUsername("postgres");
-        config.setPassword("Hbgtbh1234");
+        String[] infoPostgres = readFiles();
+        config.setJdbcUrl(infoPostgres[0]);
+        config.setUsername(infoPostgres[1]);
+        config.setPassword(infoPostgres[2]);
         ds = new HikariDataSource(config);
+    }
+
+    private static String[] readFiles(){
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            return br.readLine().split("[ ]");
+        }catch(IOException e){
+            throw new RuntimeException();
+        }
     }
 
     public static Connection getConnection() {
--- Users/src/main/java/com/users/users/repository/UserRepository.java
+++ Users/src/main/java/com/users/users/repository/UserRepository.java
@@ -23,101 +23,104 @@ public class UserRepository
     private static final String SELECT_JOIN_QUERY = "select users.id, users.name, users.role_id, roles.name from users left join roles on users.role_id = roles.id;";
 
     public boolean add(User user) {
-        var connection = DataSource.getConnection();
-        try(var statement = connection.prepareStatement(INSERT_QUERY)) {
-            statement.setString(1, user.getName());
-            statement.setInt(2, user.getRole_id());
-            return statement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return executeQuery(INSERT_QUERY, statement -> {
+            try {
+                statement.setString(1, user.getName());
+                statement.setInt(2, user.getRole_id());
+                return statement.execute();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-
     public boolean remove(int id) {
-        var connection = DataSource.getConnection();
-        try(var statement = connection.prepareStatement(DELETE_QUERY)) {
-            statement.setInt(1, id);
-            return statement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return executeQuery(DELETE_QUERY , statement -> {
+            try {
+                statement.setInt(1, id);
+                return statement.execute();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public boolean update(User user) {
-        var connection = DataSource.getConnection();
-        try(var statement = connection.prepareStatement(UPDATE_QUERY)) {
-            statement.setString(1, user.getName());
-            statement.setInt(2, user.getRole_id());
-            statement.setInt(3, user.getId());
-            statement.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return executeQuery(UPDATE_QUERY, statement -> {
+            try {
+                statement.setString(1, user.getName());
+                statement.setInt(2, user.getRole_id());
+                statement.setInt(3, user.getId());
+                statement.executeUpdate();
+                return true;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
-
 
     public List<User> getByName(String name) {
-        var connection = DataSource.getConnection();
-        try(var statement = connection.prepareStatement(SELECT_WHERE_QUERY)) {
-            statement.setString(1, name);
-            try(var set = statement.executeQuery()) {
-                List<User> users = new ArrayList<>();
-                while(set.next()){
-                    User user = new User();
-                    user.setId(set.getInt(1));
-                    user.setName(set.getString(2));
-                    user.setRole_id(set.getInt(3));
-                    users.add(user);
+        return executeQuery(SELECT_WHERE_QUERY, statement -> {
+            try {
+                statement.setString(1, name);
+                try (var set = statement.executeQuery()) {
+                    List<User> users = new ArrayList<>();
+                    while (set.next()) {
+                        User user = new User();
+                        user.setId(set.getInt(1));
+                        user.setName(set.getString(2));
+                        user.setRole_id(set.getInt(3));
+                        users.add(user);
+                    }
+                    return users;
                 }
-                return users;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        });
     }
 
-
     public List<UserRole> getAllJoined() {
-        var connection = DataSource.getConnection();
-        try(var statement = connection.prepareStatement(SELECT_JOIN_QUERY)) {
-            try(var set = statement.executeQuery()) {
-                List<UserRole> users = new ArrayList<>();
-                while(set.next()){
-                    User user = new User(set.getInt(1), set.getString(2),set.getInt(3));
-                    Role role = new Role(set.getString(4));
-                    UserRole userRole = new UserRole(user, role);
-                    users.add(userRole);
+        return executeQuery(SELECT_JOIN_QUERY, statement -> {
+            try {
+                try (var set = statement.executeQuery()) {
+                    List<UserRole> users = new ArrayList<>();
+                    while (set.next()) {
+                        User user = new User(set.getInt(1), set.getString(2), set.getInt(3));
+                        Role role = new Role(set.getString(4));
+                        UserRole userRole = new UserRole(user, role);
+                        users.add(userRole);
+                    }
+                    return users;
                 }
-                return users;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        });
     }
 
     public List<User> getAll() {
-        var connection = DataSource.getConnection();
-        try(var statement = connection.prepareStatement(SELECT_QUERY)) {
-            try(var set = statement.executeQuery()) {
-                var users = new ArrayList<User>();
-                while (set.next()) {
-                    var user = new User();
-                    user.setId(set.getInt(1));
-                    user.setName(set.getString(2));
-                    user.setRole_id(set.getInt(3));
-                    users.add(user);
+        return executeQuery(SELECT_QUERY, statement -> {
+            try {
+                try (var set = statement.executeQuery()) {
+                    var users = new ArrayList<User>();
+                    while (set.next()) {
+                        var user = new User();
+                        user.setId(set.getInt(1));
+                        user.setName(set.getString(2));
+                        user.setRole_id(set.getInt(3));
+                        users.add(user);
+                    }
+                    return users;
                 }
-                return users;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        });
     }
 
 
-    private <T> T executeQuery(String query, Function<PreparedStatement, T> function) {
+    private <T> T executeQuery(String query, Function<PreparedStatement, T> function){
         try (var connection = DataSource.getConnection();
              var statement = connection.prepareStatement(query)) {
              return function.apply(statement);
